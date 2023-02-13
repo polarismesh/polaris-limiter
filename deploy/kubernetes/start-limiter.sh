d0922dbf89f748417cc20f8a8f98354b9db02b68
chuntaojun <liaochuntao@live.com>
2023-02-13 16:00:14 +0800

    fix:limiter run docker fail

--- deploy/kubernetes/start-limiter.sh
+++ deploy/kubernetes/start-limiter.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-HOST=`hostname -s`
+LIMITER_MY_ID=${MY_ID}
 
-echo "CURRENT POD HOSTNAME : ${HOST}"
-
-if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
-    NAME=${BASH_REMATCH[1]}
-    ORD=${BASH_REMATCH[2]}
-else
-    echo "Fialed to parse name and ordinal of Pod"
-    exit 1
+if [ ${LIMITER_MY_ID} == "" ]; then
+    HOST=`hostname -s`
+    echo "CURRENT POD HOSTNAME : ${HOST}"
+    if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
+        NAME=${BASH_REMATCH[1]}
+        ORD=${BASH_REMATCH[2]}
+    else
+        echo "Fialed to parse name and ordinal of Pod"
+        exit 1
+    fi
+    LIMITER_MY_ID=$((ORD+1))
+    echo "CURRENT POD MY_ID : ${LIMITER_MY_ID}"
 fi
 
-MY_ID=$((ORD+1))
-
-echo "CURRENT POD MY_ID : ${MY_ID}"
-
 # 导出环境变量
-export MY_ID=${MY_ID}
+export MY_ID=${LIMITER_MY_ID}
 
 # 格式化 /root/polaris-limiter.yaml 文件
 envsubst </root/polaris-limiter.yaml.example >/root/polaris-limiter.yaml
